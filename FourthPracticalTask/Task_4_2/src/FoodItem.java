d108e45f1c5929f06b13f40c9a56e9de8035d81e
Telecaster525 <good.day.just.chill@gmail.com>
2024-02-11 22:46:08 +0300

    fix: fixed FoodItem constructor

--- FourthPracticalTask/Task_4_2/src/FoodItem.java
+++ FourthPracticalTask/Task_4_2/src/FoodItem.java
@@ -4,7 +4,7 @@ public class FoodItem extends GenericItem {
     Date expirationDate;
     short discount;
 
-    public FoodItem(String name, double price, Category category, Date expirationDate, short discount) {
+    public FoodItem(String name, float price, Category category, Date expirationDate, short discount) {
         super(name, price, category);
         this.expirationDate = expirationDate;
         this.discount = discount;
